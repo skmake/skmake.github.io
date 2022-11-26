957bf040f167e3c79d355dc31566733d3ced84cc
skmake <110693352+skmake@users.noreply.github.com>
2022-11-26 13:09:15 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,52 +6,7 @@ const url = "https://raw.githubusercontent.com/skmake/skmake.github.io/main/data
 
 async function getData() {
 
-    // Part 1: Get CSV
 
-    // fetch data
-    const response = await fetch(url);
-    // process data
-    const rawData = await response.text();
-    // publish data
-    document.getElementById("csv").innerHTML = rawData;
-
-    // developer info
-    console.log(rawData);
-    console.log("rawData type: " + typeof rawData);
-
-
-
-    // Part 2: CSV to JSON
-    // credit: Stack Overflow User "Paath" on 2016-05-07
-
-    // initialize variables
-    let arrayOne = rawData.split("\r\n");
-    let header = arrayOne[0].split(",");
-    let noOfRow = arrayOne.length;
-    let noOfCol = header.length;
-    let jsonData = [];
-    let i = 0;
-    let j = 0;
-
-    // for loop (rows)
-    for (i = 1; i < noOfRow - 1; i++) {
-        let obj = {};
-        let myNewLine = arrayOne[i].split(",");
-        // nested for loop (columns)
-        for (j = 0; j < noOfCol; j++) {
-            obj[header[j]] = myNewLine[j];
-        };
-        // generate JSON
-        jsonData.push(obj);
-    };
-
-    // publish data
-    document.getElementById("json").innerHTML = jsonData;
-
-    // developer info
-    console.log(jsonData);
-    console.table(jsonData);
-    console.log("jsonData type: " + typeof jsonData);
 
     // Part 3: JSON to HTML Table
     // credit: Stack Overflow User "ray hatfield" on 2015-05-26
